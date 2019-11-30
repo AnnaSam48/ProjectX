0e75defc06087578d483743bf4e19e0cfb6eeafb
rsald <rsald@DESKTOP-0LE22M2>
2019-11-30 12:50:57 +0200

    First UserInputOutput commit try

--- src/UserInputOutput.java
+++ src/UserInputOutput.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
 public class UserInputOutput {
+	public static void main(String[] args) {
+		//UserInputOutput userInputOutput = new UserInputOutput ();
+		
+		Scanner move = new Scanner(System.in);
+		System.out.println("Please move with letters: 'a' 's' 'd' or 'w'");
+		char userInput = move.nextLine().charAt(0);
+		int score = 0;
+		System.out.println(userInput); 
+		
+		//public void endGame () {
+			//System.out.println("Congratulations");
+		//}
 
+	
+			
+	}
 }
