f0c8e57bee8e9932760560914802b0a4493c7375
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2019-11-30 13:42:14 +0200

    added getUserInput()method in UserInputOutput Class

--- src/Game.java
+++ src/Game.java
@@ -1,29 +1,57 @@
-import java.util.Random;
-
 public class Game {
     public static void main(String[] args) {
 
 
-        final int rowCount = 4;
-        final int columnCount = 4;
-        int[][] board = new int[rowCount][columnCount];
+        final int ROWCOUNT = 4;
+        final int COLUMNCOUNT = 4;
+        int[][] board = new int[ROWCOUNT][COLUMNCOUNT];
         int tilesWithValue = 0;
-        boolean gameIsFinished = false;
+        boolean boardIsEmpty = true;
         int newTileGenerated = 2;
+        Game newGame = new Game();
+        UserInputOutput userInputOutput = new UserInputOutput();
 
+       do {
+            newGame.createTile();
+           char userMoveInput = userInputOutput.getUserInput();
 
-            while (!gameIsFinished) {
-                int rowIndex = (int) (Math.random() * (rowCount));
-                int columnIndex = (int) (Math.random() * (columnCount));
-                if (board[rowIndex][columnIndex] == 0) {
-                    board[rowIndex][columnIndex] = newTileGenerated;
-                    tilesWithValue++;
-                    System.out.print(board[rowIndex][columnIndex]);
-                    if (tilesWithValue == 16) {
-                        gameIsFinished = true;
-                    }
-                }
+            if (userMoveInput.equals('w')) {
+                newGame.moveUp();
+
+            } else if (userMoveInput.equals('s')) {
+                newGame.moveDown();
+
+            } else if (userMoveInput.equals('a')) {
+                newGame.moveLeft();
+
+            } else if (userMoveInput.equals('d')) {
+                newGame.moveRight();
+
+            } else {
+            throw new IllegalArgumentException("Invalid input, please use 'a', 's', 'd', 'w'");
             }
+       }
+//
+//
+////
+////                if((userInput.equals('w')||(userInput.equals('a')) || (userInput.equals('s') ||(userInput.equals('d'))){
+////                    isValid = true;
+//                    return userInput;
+////
+//        }
+//            while (boardIsEmpty) {
+//                int rowIndex = (int) (Math.random() * (ROWCOUNT -1));
+//                int columnIndex = (int) (Math.random() * (COLUMNCOUNT -1));
+//                if (board[rowIndex][columnIndex] == 0) {
+//                    board[rowIndex][columnIndex] = newTileGenerated;
+//                    tilesWithValue++;
+//                    if (tilesWithValue == 16) {
+//                        boardIsEmpty = false;
+//                        userInputOutput.endGame();
+//                        //add method from UserInputOutput Class with end game message and final score
+//                    }
+//                }
+//            }
 
 
     }
--- src/UserInputOutput.java
+++ src/UserInputOutput.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
 public class UserInputOutput {
-	
-	public void endGame () {
-		System.out.println("Congratulations!");
-		System.out.println("Your score: ");
-	}
-	
-	public static void main(String[] args) {
-		Game newGame = new Game();
-		Scanner move = new Scanner(System.in);
-		System.out.println("Please move with letters: 'a' 's' 'd' or 'w'");
-		char userInput = move.nextLine().charAt(0);
-		int score = 0;
-		//System.out.println(userInput); 			
-	}
-}
+
+    public void endGame () {
+        System.out.println("Congratulations!");
+        System.out.println("Your score: ");
+    }
+
+    //public static void main(String[] args) {
+
+
+    Game newGame = new Game();
+    public char getUserInput(){
+        Scanner move = new Scanner(System.in);
+        System.out.println("Please move with letters: 'a' 's' 'd' or 'w'");
+        char userInput = move.nextLine().charAt(0);
+        return userInput;
+    }
+
+
+    }
+//}
