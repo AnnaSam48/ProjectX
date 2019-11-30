6d9db12040eda14ec3e9a0261ecfd5a4ac27eb2f
rsald <rsald@DESKTOP-0LE22M2>
2019-11-30 13:10:17 +0200

    endGame method added

--- src/UserInputOutput.java
+++ src/UserInputOutput.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 public class UserInputOutput {
+	
+	public void endGame () {
+		System.out.println("Congratulations!");
+		System.out.println("Your score: ");
+	}
+	
 	public static void main(String[] args) {
-		//UserInputOutput userInputOutput = new UserInputOutput ();
-		
+		Game newGame = new Game();
 		Scanner move = new Scanner(System.in);
 		System.out.println("Please move with letters: 'a' 's' 'd' or 'w'");
 		char userInput = move.nextLine().charAt(0);
 		int score = 0;
-		System.out.println(userInput); 
-		
-		//public void endGame () {
-			//System.out.println("Congratulations");
-		//}
-
-	
-			
+		//System.out.println(userInput); 			
 	}
 }
