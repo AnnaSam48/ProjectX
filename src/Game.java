943d6c7a487eb05af2cb15167b11ae82bbcd0bc7
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2019-12-02 20:55:03 +0200

    Changed Interface class to Board Printer Class. Game Class errors solved. Still needs debugging.

--- src/Game.java
+++ src/Game.java
@@ -1,61 +1,65 @@
 public class Game {
 
+
     public static void main(String[] args) {
 
         Game newGame = new Game();
+        newGame.createTile();
+        newGame.printBoard();
+
         UserInputOutput userInputOutput = new UserInputOutput();
-        char userMoveInput = userInputOutput.getUserInput();
 
-        do {
-            newGame.createTile();
+ //       userInputOutput.startUpText();
+        String userMoveInput = userInputOutput.getUserInput();
 
-            if (userMoveInput == ('w')) {
+        while (!userMoveInput.equals("?")) {
+            if (userMoveInput.equals("w")) {
                 newGame.moveUp();
 
-            } else if (userMoveInput == ('s')) {
+            } else if (userMoveInput.equals("s")) {
                 newGame.moveDown();
 
-            } else if (userMoveInput == ('a')) {
+            } else if (userMoveInput.equals("a")) {
                 newGame.moveLeft();
 
-            } else if (userMoveInput == ('d')) {
+            } else if (userMoveInput.equals("d")) {
                 newGame.moveRight();
 
             } else {
-                if (userMoveInput == ('?')) {
+                if (userMoveInput.equals("?")) {
                     throw new IllegalArgumentException("Invalid input, please use 'a', 's', 'd', 'w'");
                 }
             }
-        } while (userMoveInput != ('?'));
+
+            newGame.createTile();
+            newGame.printBoard();
+
+            userMoveInput = userInputOutput.getUserInput();
+        }
     }
 
     final int ROWCOUNT = 4;
     final int COLUMNCOUNT = 4;
     int[][] board = new int[ROWCOUNT][COLUMNCOUNT];
     int tilesWithValue = 0;
-    boolean boardIsEmpty = true;
     int newTileGenerated = 2;
     UserInputOutput userInputOutput = new UserInputOutput();
 
     public void createTile() {
-
-
-        while (boardIsEmpty) {
+        
+        while (true) {
             int rowIndex = (int) (Math.random() * (ROWCOUNT - 1));
             int columnIndex = (int) (Math.random() * (COLUMNCOUNT - 1));
             if (board[rowIndex][columnIndex] == 0) {
                 board[rowIndex][columnIndex] = newTileGenerated;
                 tilesWithValue++;
-                if (tilesWithValue == 16) {
-                    boardIsEmpty = false;
-                    userInputOutput.endGame();
                     //add method from UserInputOutput Class with end game message and final score
-                }
+                return;
             }
         }
     }
 
-    private void moveVerticaly(int currentRow, int currentCol, boolean verticalMovement) {
+    private void moveVertically(int currentRow, int currentCol, boolean verticalMovement) {
 
         if (board[tilesWithValue][currentCol] == 0 ||
                 board[tilesWithValue][currentCol] == board[currentRow][currentCol]) {
@@ -70,11 +74,11 @@ public class Game {
             } else {
                 tilesWithValue++;
             }
-            moveVerticaly(currentRow, currentCol, verticalMovement);
+            moveVertically(currentRow, currentCol, verticalMovement);
         }
     }
 
-    private void moveHorizontaly(int currentRow, int currentCol, boolean verticalMovement) {
+    private void moveHorizontally(int currentRow, int currentCol, boolean verticalMovement) {
 
         if (board[currentRow][tilesWithValue] == 0 ||
                 board[currentRow][tilesWithValue] == board[currentRow][currentCol]) {
@@ -89,7 +93,7 @@ public class Game {
             } else {
                 tilesWithValue++;
             }
-            moveHorizontaly(currentRow, currentCol, verticalMovement);
+            moveHorizontally(currentRow, currentCol, verticalMovement);
         }
     }
 
@@ -98,7 +102,7 @@ public class Game {
             for (int j = 0; j < ROWCOUNT; j++) {
                 if (board[j][i] != 0) {
                     if (tilesWithValue <= j) {
-                        moveVerticaly(j, i, false);
+                        moveVertically(j, i, false);
                     }
                 }
             }
@@ -111,7 +115,7 @@ public class Game {
             for (int j = tilesWithValue; j >= 0; j--) {
                 if (board[j][i] != 0) {
                     if (tilesWithValue >= j) {
-                        moveVerticaly(j, i, true);
+                        moveVertically(j, i, true);
                     }
                 }
             }
@@ -124,7 +128,7 @@ public class Game {
             for (int j = 0; j < COLUMNCOUNT; j++) {
                 if (board[i][j] != 0) {
                     if (tilesWithValue <= j) {
-                        moveHorizontaly(i, j, false);
+                        moveHorizontally(i, j, false);
                     }
                 }
             }
@@ -137,14 +141,27 @@ public class Game {
             for (int j = tilesWithValue; j >= 0; j--) {
                 if (board[i][j] != 0) {
                     if (tilesWithValue >= j) {
-                        moveHorizontaly(i, j, true);
+                        moveHorizontally(i, j, true);
                     }
                 }
             }
         }
+
+    }
+    public void printBoard(){
+        String output = "";
+        for (int i = 0; i < ROWCOUNT; i++) {
+            String rowOutput = "";
+            for (int j = 0; j < COLUMNCOUNT; j++) {
+                rowOutput += board[i][j] + " ";
+            }
+            rowOutput += "\n";
+            output += rowOutput;
+        }
+        System.out.println(output);
     }
 }
 //
 //                if((userInput.equals('w')||(userInput.equals('a')) || (userInput.equals('s') ||(userInput.equals('d'))){
 //                    isValid = true;
-//                    return userInput;
+//                    return userI/////nput;
--- src/UserInputOutput.java
+++ src/UserInputOutput.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 
 public class UserInputOutput {
-    UserInputOutput userInputOutput = new UserInputOutput();
 
-    Game newGame = new Game();
-    public char getUserInput() {
+//    public void startUpText(){
+//        System.out.println("Hello! This is console game 2048.");
+//        System.out.println("Your task is to move tiles with numbers, so that there are two adjacent tiles with same values.");
+//        System.out.println("The tiles (according to your direction input) with same values will merge and their value will sum up.");
+//        System.out.println("Can you get to 2048? We shall see... We wish you best of luck!");
+//    }
+    public String getUserInput() {
         Scanner move = new Scanner(System.in);
         System.out.println("Please move with letters: 'a' 's' 'd' or 'w'");
-        char userInput = move.nextLine().charAt(0);
+        String userInput = move.next();
         return userInput;
     }
 
